865f74f219d4ea11d1d7e4893234c52bf9ab4c7c
César Tallys <82481963+CesarTHD@users.noreply.github.com>
2024-11-20 12:40:52 -0300

    Voltando credeciais do firebase para main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stepmd/app/app_widget.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'firebase_options.dart';
 
 import 'app/app_module.dart';
 
@@ -11,7 +9,15 @@ void main() async {
   WidgetsFlutterBinding
       .ensureInitialized(); // Garante que o binding do Flutter esteja pronto.
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyB-AUxlEBBUIl5Hsqs82p-V2v9o1tfZIbw",
+      authDomain: "stepmd-ebcd9.firebaseapp.com",
+      projectId: "stepmd-ebcd9",
+      storageBucket: "stepmd-ebcd9.appspot.com",
+      messagingSenderId: "562194623173",
+      appId: "1:562194623173:web:a2fcbb97b40a7bb66d5eac",
+      measurementId: "G-FNB4FM5CWM"
+    ),
   );
 
   runApp(ModularApp(module: AppModule(), child: AppWidget()));
